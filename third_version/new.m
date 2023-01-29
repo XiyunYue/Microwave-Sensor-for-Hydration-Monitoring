ca821d906dcbd1e15dc60da28357f372d619f2be
xliu2 <xliu2@tcd.ie>
2023-01-29 13:45:08 +0000

    Including the calculation of 3dB bandwidth and phase

--- third_version/new.m
+++ third_version/new.m
@@ -1,17 +1,18 @@
-%% 系数输入
-n = 11; %数据个数
-n_start = 18;%开始天线节数
-a1 = n_start - 1;%简化调用函数
+%% Coefficient input
+n = 11; %data volume
+n_start = 18;%Minimum Antenna Sections
+a1 = n_start - 1;%Simplify calling functions
 
-%% 设置空矩阵
+%% set empty matrix
 fre_syntony_test = zeros(n, 1);
 fre_syntony_calculate = zeros(n, 1);
 amplitude_syntony_test = zeros(n, 1);
 bandwidth = zeros(n, 1);
 num_antenna = n_start : n_start + n - 1;
 color = zeros(n, 3);
+phase = zeros(n, 1);
 
-%% 设置data矩阵
+%% Set the data matrix
 file_start = strcat ('230123_dipole_',int2str(n_start) ,'_no');
 A = readmatrix(file_start);
 fre_data = zeros(length(A), 2 * n + 1);
@@ -19,12 +20,12 @@ amplitude = zeros(length(A), n);
 fre_data(:, 1) = A(:, 1);
 x = fre_data(:, 1);
 
-%% 导入参数
+%% 
 for i = n_start : n_start + n - 1
     file_name = strcat ('230123_dipole_', int2str(i) ,'_no');
     A = readmatrix(file_name);
     fre_data(:, [2 * i - a1 * 2, 2 * i - a1 * 2 + 1]) = A(:, [2, 3]);
-    [bandwidth(i - a1), amplitude(:, i - a1), ...
+    [bandwidth(i - a1), amplitude(:, i - a1), phase(i - a1), ...
        amplitude_syntony_test(i - a1), fre_syntony_test(i - a1), ...
        fre_syntony_calculate(i - a1)] = new_function(i, ...
        fre_data(:, [1, 2 * i - a1 * 2, 2 * i - a1 * 2 + 1]));
@@ -32,12 +33,12 @@ end
 
 freq_3dB = -3 + 0 * fre_data(:, 1);
 
-%% 颜色设置
+%% color settings
 for i = 1 : n
     color(i,:)=[i/n 0 1];
 end
 
-%% 画图
+%% Image display
 figure (1)
 for i=1:11
     plot(fre_data(:, 1), amplitude(:, i), 'color', color(i,:));
@@ -71,6 +72,12 @@ ylabel('syntony amplitude(dB)');
 title("The attenuation amplitude when resonates")
 
 figure (4)
+plot(num_antenna, phase);
+xlabel('The number of nodes of the antenna');
+ylabel('syntony phase(rad)');
+title("The attenuation phase when resonates")
+
+figure (5)
 plot(num_antenna, bandwidth);
 xlabel('The number of nodes of the antenna');
 ylabel('bandwidth');
