d3ec29825590475cafe11b0569612a5c681147d7
xliu2 <xliu2@tcd.ie>
2023-01-30 17:22:17 +0000

    Changed the 3dB calculate function

--- third_version/new_function.m
+++ third_version/new_function.m
@@ -1,4 +1,4 @@
-function [bandwidth, amplitude, amplitude_syntony_test, fre_syntony_test, fre_syntony_calculate] = new_function(n, dipole)
+function [bandwidth, amplitude, phase_syntony_test, amplitude_syntony_test, fre_syntony_test, fre_syntony_calculate] = new_function(n, dipole)
 c = 3 * (10 ^ 8);
 wavelength = (4 + 2 * 3 * n + 1.5) * 2 * 10 ^ ( - 3);
 freq = dipole(:, 1);
@@ -7,20 +7,30 @@ amplitude_image = dipole(:, 3);
 amplitude = 10 * log(sqrt(amplitude_real .^ 2 + amplitude_image .^ 2));
 
 
-d = amplitude ./ (-3);
-freq_3dB_point = freq(find(d > .99 & d < 1.01));
-% amplitude_3dB = amplitude + 3;
-% figure (n - 17)
-% plot(freq, amplitude,'r')
-% freq_3dB = -3 + 0 * freq;
-% d = amplitude ./ 3;
-% freq_3dB_point1 = find(d > .999 & d < 1.001  & freq < 1 * 10 ^ 9);
-% freq_3dB_point = @(freq) amplitude(freq) - freq_3dB(freq);
-% freq_3dB_point1 = freq_3dB_point(1);
+% d = amplitude ./ (-3);
+% freq_3dB_point = freq(find(d > .99 & d < 1.01));
+% % amplitude_3dB = amplitude + 3;
+% % figure (n - 17)
+% % plot(freq, amplitude,'r')
+% % freq_3dB = -3 + 0 * freq;
+% % d = amplitude ./ 3;
+% % freq_3dB_point1 = find(d > .999 & d < 1.001  & freq < 1 * 10 ^ 9);
+% % freq_3dB_point = @(freq) amplitude(freq) - freq_3dB(freq);
+% % freq_3dB_point1 = freq_3dB_point(1);
+% bandwidth = freq_3dB_point(2) - freq_3dB_point(1);
+
+sit = find(amplitude == min(amplitude));
+fre_syntony_test = freq(sit);
+amplitude_syntony_test = amplitude(sit);
+amplitude_3dB = amplitude_syntony_test + 3;
+d = amplitude ./ amplitude_3dB;
+% figure(n)
+% plot(freq, d)
+freq_3dB_point = freq(find(d > .9 & d < 1.1));
 bandwidth = freq_3dB_point(2) - freq_3dB_point(1);
 
-fre_syntony_test = freq(find(amplitude == min(amplitude)));
-amplitude_syntony_test = amplitude(find(amplitude == min(amplitude)));
+phase_syntony_test = (180 / pi) * atan(amplitude_image(sit) / ...
+    amplitude_real(sit));
 % freq_3dB = freq(find(abs(amplitude_3dB) == min(abs(amplitude_3dB))));
 % bandwidth = freq_3dB(1) - freq_3dB(2);
 % d = freq_3dB ./ 3;
